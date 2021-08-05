d0e7fe75b5d3223cd2924c23a8d9540ec0553716
Szwedu1221 <56169063+Szwedu1221@users.noreply.github.com>
2021-08-05 15:07:45 +0200

    Create Spoj.cpp

---
+++ LiczbyPierwsze/Spoj.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <cmath>
+
+	using namespace std;
+
+
+
+	int main()
+	{
+		int n = 10000;
+		bool tab[10001];
+		for (int i = 0; i <= n; i++)
+		{
+			tab[i] = true;
+
+		}
+		tab[1] = false;
+		for (int i = 2; i <= sqrt(n); i++)
+		{
+			if (tab[i] == true)
+			{
+				for (int j = i + i; j <= n; j = j + i)
+				{
+					tab[j] = false;
+				}
+			}
+		}
+
+		int ile_testow;
+		cin >> ile_testow;
+		for (int i = 0; i < ile_testow; i++)
+		{
+			int liczba;
+			cin >> liczba;
+			if (tab[liczba] == true)
+			{
+				cout << "TAK" << endl;
+			}
+			else
+			{
+				cout << "NIE" << endl;
+			}
+		}
+		return 0;
+	}
