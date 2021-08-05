88b5c6f65eb2e0c6acb833ad91b1e538e869cde4
Szwedu1221 <56169063+Szwedu1221@users.noreply.github.com>
2021-08-05 15:18:13 +0200

    Create Spoj.cpp

---
+++ Proste Dodawanie/Spoj.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+	unsigned int t;
+	int n;
+	cin >> t;
+	int wynik1 = 0;
+	for (int i = 0; i < t; i++)
+	{
+		cin >> n;
+		int* wynik = new int[n];
+		for (int j = 0; j < n; j++)
+		{
+			cin >> wynik[j];
+			wynik1 = wynik1 + wynik[j];
+		}
+		cout << wynik1 << endl;
+		wynik1 = 0;
+	}
+	return 0;
+}
