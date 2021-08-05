5931451f3e9eefcfb6386b2bc6ba821b5b1acd05
Szwedu1221 <56169063+Szwedu1221@users.noreply.github.com>
2021-08-05 15:20:43 +0200

    Create Spoj.cpp

---
+++ NWD/Spoj.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+
+using namespace std;
+
+int nwd(int a, int b)
+{
+	while (a != b)
+	{
+		if (a > b)
+		{
+			a -= b;
+		}
+		else
+		{
+			b -= a;
+		}
+	}
+	return a;
+}
+int main()
+{
+	int a, b, t;
+	cin >> t;
+	for (int i = 0; i < t; i++)
+	{
+		cin >> a >> b;
+		cout << nwd(a, b) << endl;
+	}
+
+
+	return 0;
+}
+
